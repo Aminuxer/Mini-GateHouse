b85122e92fbc8a2055b6f36c32a60e91c56cae2a
Amin <Aminuxer@users.noreply.github.com>
2022-06-29 17:41:19 +0300

    Create options.php

---
+++ www/options.php
@@ -0,0 +1,16 @@
+<?php
+/* ------------------------- */
+/*   MySQL connect options   */
+
+$db_hoster = 'localhost' ;
+$db_name = 'gatehouse' ;
+
+$db_login = 'gatehouse' ;
+$db_pwd = 'gatehousepassword' ;
+/* ------------------------- */
+/* Additional RANDOM server-side key for crypting cookies */
+/* CHANGE IT AFTER INSTALLATION */
+$db_add_rnd_key = '00000000000000080000000000000000000000900000000000';
+
+/* ------------------------- */
+?>
